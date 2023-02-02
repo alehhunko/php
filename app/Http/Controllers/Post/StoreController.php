52fa9527f7dd976b9e85a5e1b98a930fc013e9da
alehhunko <teplohouses@gmail.com>
2023-02-02 16:47:30 +0300

    10-2

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -5,19 +5,14 @@ namespace App\Http\Controllers\Post;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use Illuminate\Support\Facades\Redirect;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'post_content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tagss = $data['tags'];
         unset($data['tags']);
         $post = Post::create($data);
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -5,19 +5,14 @@ namespace App\Http\Controllers\Post;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use Illuminate\Support\Facades\Redirect;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(Post $post, UpdateRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'post_content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = $request->validated();
         $tagss = $data['tags'];
         unset($data['tags']);
         $post->update($data);
