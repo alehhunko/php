4db6a5b651d9df53f8b418e4e55c3fd9900ab952
alehhunko <teplohouses@gmail.com>
2023-02-10 13:42:05 +0300

    17

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,22 +11,25 @@
 
 <body class="container">
     <div class="row">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="{{route('main.index')}}">Main <span
-                        class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="{{route('post.index')}}">Posts</a>
-                <a class="nav-item nav-link" href="{{route('about.index')}}">About</a>
-                <a class="nav-item nav-link" href="{{route('contact.index')}}">Contacts</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="{{route('main.index')}}">AdminLTE<span
+                            class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="{{route('post.index')}}">Posts</a>
+                    <a class="nav-item nav-link" href="{{route('about.index')}}">About</a>
+                    <a class="nav-item nav-link" href="{{route('contact.index')}}">Contacts</a>
+                    @can('view', auth()->user())
+                    <a class="nav-item nav-link" href="{{route('admin.post.index')}}">Admin</a>
+                    @endcan
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     </div>
     @yield('contents')
 </body>
